3749a7a0c83c7ce1e4cf395959ba040149f5ba7d
bondalina <142501520+bondalina@users.noreply.github.com>
2024-02-04 12:49:51 +0200

    check1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-
-// Описаний у документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 import axios from "axios";
 
 const searchForm = document.querySelector('.search-form');
@@ -17,15 +11,12 @@ const loader = document.querySelector('.loader');
 const loadMoreBtn = document.querySelector('.load-more');
 
 const lightbox = new SimpleLightbox('.gallery a');
-
 let currentPage = 1;
 let currentQuery = '';
 
 searchForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-
     const inputValue = searchInput.value.trim();
-
     if (inputValue === '') {
         iziToast.error({
             title: 'Error',
@@ -34,17 +25,12 @@ searchForm.addEventListener('submit', async (event) => {
         });
         return;
     }
-
-    // Показати індикатор завантаження
     loader.style.display = 'block';
     loadMoreBtn.style.display = 'none';
-
     currentQuery = inputValue;
     currentPage = 1;
-    
     galleryList.innerHTML = '';
     
-
     await searchImages(currentQuery, currentPage);
 });
 
@@ -52,19 +38,17 @@ loadMoreBtn.addEventListener('click', async () => {
     currentPage += 1;
     loader.style.display = 'block';
     await searchImages(currentQuery, currentPage);
+    smoothScroll();
 });
 
 async function searchImages(query, page) {
     const apiKey = '36996517-56800863ae540be6945d0f4f2';
     const perPage = 15;
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
     try {
         const response = await axios.get(url);
         const data = response.data;
-
         searchInput.value = '';
-
         if (data.hits.length === 0) {
             iziToast.error({
                 message: 'Sorry, there are no images matching your search query. Please try again!',
@@ -73,12 +57,8 @@ async function searchImages(query, page) {
             });
             loader.style.display = 'none';
             return;
-        }
-
-
-        setTimeout(() => {
-            galleryList.innerHTML += data.hits.map(image => {
-            const imageElement = 
+        }     
+        const imageElements = data.hits.map(image =>              
             `<li class="gallery-item">
                 <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
                     <img class="img-item" src="${image.webformatURL}" alt="${image.tags}">
@@ -89,22 +69,25 @@ async function searchImages(query, page) {
                     <li>Comments <br/>${image.comments}</li>
                     <li>Downloads <br/>${image.downloads}</li>
                 </ul>
-            </li>`;
-            return imageElement;
-        }).join('');
-
+            </li>`           
+        ).join('');
+        galleryList.insertAdjacentHTML("beforeend", imageElements)
         
-
         loader.style.display = 'none';
-        lightbox.refresh();
-    
+        lightbox.refresh(); 
 
-        if (data.totalHits > page * perPage) {
-            loadMoreBtn.style.display = 'block';
-        } else {
+        const totalResults = data.totalHits;
+        if (totalResults <= page * perPage) {
             loadMoreBtn.style.display = 'none';
+                    iziToast.error({
+                    message: "We're sorry, but you've reached the end of search results.",
+                    position: 'topRight',
+                    timeout: 3000,
+                });
+            }
+        else {
+            loadMoreBtn.style.display = 'block';
         }
-    }, 1000); // Зробила затримку відображення зображень на 1 секунди
 
 }   catch (error) {
         iziToast.error({
@@ -114,3 +97,14 @@ async function searchImages(query, page) {
         });
         loader.style.display = 'none';
     }}
+
+// Плавна прокрутка сторінки
+function smoothScroll() {
+    // Отримати висоту однієї карточки галереї
+    const galleryItemHeight = document.querySelector('.gallery-item').getBoundingClientRect().height;
+    // Плавна прокрутка сторінки
+    window.scrollBy({
+        top: galleryItemHeight * 3, // на дві висоти карточки
+        behavior: 'smooth' 
+        });
+    }
